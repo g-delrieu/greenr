1ca4ebd42f205b00f51319979e02a7100b3f2a3d
georges delrieu <georges.delrieu@laposte.net>
2020-09-01 19:08:27 +0200

    fixed file location

--- greenr/app.py
+++ greenr/app.py
@@ -49,7 +49,7 @@ def set_png_as_page_bg(png_file):
     st.markdown(page_bg_img, unsafe_allow_html=True)
     return
 
-set_png_as_page_bg('background.png')
+set_png_as_page_bg('greenr/background.png')
 ###########################################
 
 def local_css(file_name):
