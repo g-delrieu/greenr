f86735e0321039e8e83972811b90bcd8c3864d84
georges delrieu <georges.delrieu@laposte.net>
2020-09-03 16:53:30 +0200

    changed for french and eng

--- greenr/main_calculation.py
+++ greenr/main_calculation.py
@@ -12,12 +12,28 @@ import pandas as pd
 import scraper_parser
 import matching
 import calculator
+from urllib.parse import urlparse
+
 
 
 
 def calculate(url):
 
-    df_parsed, servingsize, raw_ingredient_list, recipe_title = scraper_parser.url_to_df(url)
+    o = urlparse(url)
+    if o.netloc == 'www.bbc.co.uk':
+        try:
+            df_parsed, servingsize, raw_ingredient_list, recipe_title = scraper_parser.url_to_df(url)
+        except:
+            print('invalid input')
+
+    elif o.netloc == 'www.marmiton.org':
+        try:
+            df_parsed = scraper_parser.marmiton_to_df(url)
+        except:
+            print('invalid input')
+    else:
+        print('invalid input')
+
 
     categories = matching.get_categories(df_parsed, try_google = True)
 
@@ -28,4 +44,9 @@ def calculate(url):
     df_parsed['impact'] = impact_list
     df_parsed['raw_ingredient'] = raw_ingredient_list[:-1]
 
-    return round(ghg_impact_sum/int(servingsize),1), df_parsed, recipe_title
+    if servingsize:
+        out = (round(ghg_impact_sum/int(servingsize),1), df_parsed, recipe_title, True)
+    else:
+        out = (round(ghg_impact_sum,1), df_parsed, False)
+
+    return out
