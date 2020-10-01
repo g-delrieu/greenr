62a551cd9296be1bff892cc24892956936194708
georges delrieu <georges.delrieu@laposte.net>
2020-10-01 17:43:51 +0200

    improving api

--- greenr/api.py
+++ greenr/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, jsonify
 import main_calculation
 
 app = Flask(__name__)
@@ -8,7 +8,10 @@ def login():
     if request.method == "POST":
         url = request.form['url']
         score = main_calculation.calculate(url)
-        return score
+        return jsonify({"footprint per serving" : score[0],
+                        "all_data" : score[1].to_json(orient="split"),
+                        "title": score[3],
+                        "url": score[4]})
     else:
         return render_template("login.html")
 
